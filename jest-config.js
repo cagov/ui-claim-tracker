151694fc93a3b2caacec55f526e052d666a44dfb
Kat Tipton <kat@navapbc.com>
2021-03-16 11:53:34 -0400

    Adds enzyme, configs from cagov/unemployment

--- jest-config.js
+++ jest-config.js
@@ -1,10 +1,21 @@
+/**
+ * In order for Azure DevOps artifacts to be published for test results,
+ * and code coverage metrics, we need to use jest-junit and cobertura
+ * test and coverage reporters respectively.
+ */
+const outputDirectory = "<rootDir>/test-output";
 module.exports = {
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
+  coverageDirectory: outputDirectory,
+  coverageReporters: ["cobertura"],
+  reporters: ["default", ["jest-junit", { outputDirectory }]],
+  roots: ["<rootDir>/src/"],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
