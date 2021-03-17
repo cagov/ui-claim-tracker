426c1ed1350c4d83ce279adc70fa4db77666d1cc
Kat Tipton <kat@navapbc.com>
2021-03-17 10:58:52 -0400

    WIP - debugging adding a test, babel not functional

--- jest-config.js
+++ jest-config.js
@@ -1,25 +1,14 @@
-/**
- * In order for Azure DevOps artifacts to be published for test results,
- * and code coverage metrics, we need to use jest-junit and cobertura
- * test and coverage reporters respectively.
- */
-const outputDirectory = "<rootDir>/test-output";
 module.exports = {
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
-  coverageDirectory: outputDirectory,
-  coverageReporters: ["cobertura"],
-  reporters: ["default", ["jest-junit", { outputDirectory }]],
-  roots: ["<rootDir>/src/"],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^.+\\.css$': 'jest-transform-stub',
   },
   transformIgnorePatterns: [
     '/node_modules/',
@@ -30,3 +19,8 @@ module.exports = {
   },
 }
 
+//const outputDirectory = "<rootDir>/test-output";
+  //snapshotSerializers: ["enzyme-to-json/serializer"],
+  //coverageDirectory: outputDirectory,
+  //coverageReporters: ["cobertura"], //for Azure DevOps
+  //reporters: ["default", ["jest-junit", { outputDirectory }]], //for Azure DevOps
