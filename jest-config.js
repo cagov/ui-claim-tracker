0ed0831f574d5d75034647c787e3b958b21375e5
Kat Tipton <kat@navapbc.com>
2021-03-17 17:48:40 -0400

    Functional Jest tests

--- jest-config.js
+++ jest-config.js
@@ -1,23 +1,23 @@
-module.exports = {
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': 'jest-transform-stub',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
-  moduleNameMapper: {
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-  },
-}
+//module.exports = {
+//  collectCoverageFrom: [
+//    '**/*.{js,jsx,ts,tsx}',
+//    '!**/*.d.ts',
+//    '!**/node_modules/**',
+//  ],
+//  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+//  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+//  transform: {
+//    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+//    '^.+\\.css$': 'jest-transform-stub',
+//  },
+//  transformIgnorePatterns: [
+//    '/node_modules/',
+//    '^.+\\.module\\.(css|sass|scss)$',
+//  ],
+//  moduleNameMapper: {
+//    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+//  },
+//}
 
 //const outputDirectory = "<rootDir>/test-output";
   //snapshotSerializers: ["enzyme-to-json/serializer"],
