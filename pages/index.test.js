a7411550301c54ee80fd804cb34187dca45e09d4
Kat Tipton <kat@navapbc.com>
2021-03-18 17:36:42 -0400

    Adds testing library for react and react test renderer

--- pages/index.test.js
+++ pages/index.test.js
@@ -12,9 +12,21 @@
 
 import React from 'react'
 import renderer from 'react-test-renderer'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Index from '../pages/index'
 
-it('renders homepage unchanged', () => {
-  const tree = renderer.create(<Index />).toJSON()
-  expect(tree).toMatchSnapshot()
+describe("Exemplar-react-test renderer Snapshot test", () => {
+  it('renders homepage unchanged', () => {
+    const tree = renderer.create(<Index />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+
+describe("Example react testing-library Test", () => {
+  it('has NextJS welcome text', () => {
+    render(<Index />)
+    expect(screen.queryByText("Welcome to").toBeInTheDocument())
+  })
 })
