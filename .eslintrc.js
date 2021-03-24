3d74a6db0e8eaa10ec7793857315633eff7c4232
Kat Tipton <kat@navapbc.com>
2021-03-24 14:50:13 -0400

    Adds Nava standard & ignores irrelevent files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es6: true
   },
   extends: [
     'plugin:react/recommended',
-    'standard'
+    'nava'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
