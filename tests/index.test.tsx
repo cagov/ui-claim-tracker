cec19d6ccf17ddc34989acd5797da7f3f91f95dd
Kat Tipton <kat@navapbc.com>
2021-03-26 10:12:39 -0400

    Pretties the test file

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import {render, screen} from '@testing-library/react'
-import Index from '../pages/index'
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import Index from '../pages/index';
 
-describe("Exemplar-react-test renderer Snapshot test", () => {
+describe('Exemplar-react-test renderer Snapshot test', () => {
   it('renders homepage unchanged', () => {
-    const tree = renderer.create(<Index />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
+    const tree = renderer.create(<Index />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-
-describe("Example react testing-library Test", () => {
+describe('Example react testing-library Test', () => {
   it('has our placeholder app', () => {
-    render(<Index />)
-    expect(screen.queryByText("Welcome to the Placeholder Claim Tracker App")).toBeInTheDocument()
-  })
-})
+    render(<Index />);
+    expect(screen.queryByText('Welcome to the Placeholder Claim Tracker App')).toBeInTheDocument();
+  });
+});
