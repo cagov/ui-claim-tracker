197d2a97a47f56b9463508b336cffb1ed940fa5d
Kat Tipton <kat@navapbc.com>
2021-03-26 11:09:06 -0400

    Revert "Test husky"
    This reverts commit b25e5be4f5fffbe7260b1a359c08ea4674476cd7.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <div>
-          <iframe width="561" height="315" src="https://www.youtube.com/embed/EIyixC9NsLI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/EIyixC9NsLI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
 
       </main>
