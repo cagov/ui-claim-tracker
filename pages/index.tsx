0876a347fb90834c86ee736c3ca966837d61eebc
Kat Tipton <kat@navapbc.com>
2021-03-26 13:53:02 -0400

    Cleans up bad commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head"
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -10,25 +10,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to the Placeholder Claim Tracker App
-        </h1>
+        <h1 className={styles.title}>Welcome to the Placeholder Claim Tracker App</h1>
 
         <div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/EIyixC9NsLI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/EIyixC9NsLI"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
         </div>
-
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://www.navapbc.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://www.navapbc.com/" target="_blank" rel="noopener noreferrer">
           Powered by Nava PBC
         </a>
       </footer>
     </div>
-  )
+  );
 }
