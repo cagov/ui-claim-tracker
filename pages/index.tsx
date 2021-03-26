e41f03328b400e390526d07dde8e640cce75cf23
Kat Tipton <kat@navapbc.com>
2021-03-26 09:56:27 -0400

    Adds types to function sig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
+import { ReactElement } from 'react';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home(): ReactElement {
   return (
     <div className={styles.container}>
       <Head>
