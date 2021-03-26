ef083a579d6c39d3a393b8a07200049f7fbaaf8f
Kat Tipton <kat@navapbc.com>
2021-03-26 09:18:31 -0400

    Add jest dom & react eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'nava',
     'plugin:react/recommended',
     'plugin:jest/recommended',
+    'plugin:jest-dom/recommended',
     'plugin:testing-library/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
@@ -23,8 +24,14 @@ module.exports = {
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to autodetect the React version to use
+    }
+  },
   plugins: [
     'jest',
+    'jest-dom',
     'react',
     'testing-library',
     '@typescript-eslint'
