0739bf5dc3a4d31e2a12d4e785ffeb9f04290df0
Kat Tipton <kat@navapbc.com>
2021-04-20 15:04:45 -0400

    Styles the larger view

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,25 +8,33 @@ export const Footer: React.FC = () => {
 
   return (
     <footer className="footer">
-      <Navbar className="justify-content-between" variant="dark" bg="dark">
-        <Nav className="flex-wrap">
-          <Nav.Link href="#top">{t('toTop')}</Nav.Link>
-          <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/contact_edd.htm">
-            {t('contact')}
-          </Nav.Link>
-          <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/conditions_of_use.htm">
-            {t('conditionsOfUse')}
-          </Nav.Link>
-          <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/privacy_policy.htm">
-            {t('privacyPolicy')}
-          </Nav.Link>
-          <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/accessibility.htm">
-            {t('accessibility')}
-          </Nav.Link>
-        </Nav>
-      </Navbar>
-      <div className="bg-light secondary-footer">
-        <Container>{t('copyright')}</Container>
+      <Container>
+        <Navbar variant="dark">
+          <Nav className="flex-wrap">
+            <Nav.Link href="#top">{t('toTop')}</Nav.Link>
+            <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/contact_edd.htm">
+              {t('contact')}
+            </Nav.Link>
+            <Nav.Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://edd.ca.gov/about_edd/conditions_of_use.htm"
+            >
+              {t('conditionsOfUse')}
+            </Nav.Link>
+            <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/privacy_policy.htm">
+              {t('privacyPolicy')}
+            </Nav.Link>
+            <Nav.Link target="_blank" rel="noopener noreferrer" href="https://edd.ca.gov/about_edd/accessibility.htm">
+              {t('accessibility')}
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+      <div className="secondary-footer">
+        <Container>
+          <div className="copyright">{t('copyright')}</div>
+        </Container>
       </div>
     </footer>
   )
