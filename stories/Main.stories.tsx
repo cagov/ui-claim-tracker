5b09bd106729db37ab7596b0aa04421727ba959d
Kat Tipton <kat@navapbc.com>
2021-04-27 11:58:42 -0400

    Allows the router to work with storybook

--- stories/Main.stories.tsx
+++ stories/Main.stories.tsx
@@ -1,10 +1,12 @@
 import { Story, Meta } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
 
 import { Main } from '../components/Main'
 
 export default {
   title: 'component/Main',
   component: Main,
+  decorators: [withNextRouter],
 } as Meta
 
 const Template: Story = (args) => <Main {...args} />
