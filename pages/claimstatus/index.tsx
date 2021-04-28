f67a3e7c5f327ebe9d7e8c9eaee069ea0fcd0280
Kat Tipton <kat@navapbc.com>
2021-04-28 10:06:46 -0400

    Move the app to /claimstatus

--- pages/claimstatus/index.tsx
+++ pages/claimstatus/index.tsx
@@ -7,8 +7,8 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { GetServerSideProps } from 'next'
 
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
 
 export default function Home(): ReactElement {
   const { t } = useTranslation('common')
