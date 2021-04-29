aedcc119568a398de578275c41f0affb9f8bd783
Kalvin <82277+kalvinwang@users.noreply.github.com>
2021-04-29 16:40:52 -0400

    Add Claim Card component + small fixes

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,26 +1,19 @@
 import Container from 'react-bootstrap/Container'
 
-import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 
 import { Title } from './Title'
 import { Breadcrumbs } from './Breadcrumbs'
+import { ClaimCard } from './ClaimCard'
 
 export const Main: React.FC = () => {
-  const { t } = useTranslation('common')
-  const router = useRouter()
-
   return (
     <main className="main">
       <Container className="main-content">
-        <Title />
-        <p />
         <Breadcrumbs />
-        <p />
-        <Link href="/" locale={router.locale === 'en' ? 'es' : 'en'}>
-          <button>{t('change-locale')}</button>
-        </Link>
+        <Title />
+        <ClaimCard />
       </Container>
     </main>
   )
