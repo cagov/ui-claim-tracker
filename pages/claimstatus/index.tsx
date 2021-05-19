530d7ff8e758aa428d8e326e4e1f53255461628b
Kevin Moore <k.james.moore@gmail.com>
2021-05-19 19:13:18 -0400

    App is successfully connecting to API gateway, if proper .p12 file is available and proper env variables are set.

--- pages/claimstatus/index.tsx
+++ pages/claimstatus/index.tsx
@@ -1,8 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import https from 'https'
-import fetch from 'node-fetch'
-import { IncomingHttpHeaders } from 'http'
+import { promisify } from 'util'
 
 import Head from 'next/head'
 import Container from 'react-bootstrap/Container'
@@ -10,6 +9,8 @@ import { ReactElement } from 'react'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { GetServerSideProps } from 'next'
+import pem from 'pem'
+import fetch from 'node-fetch'
 
 import { Header } from '../../components/Header'
 import { Main } from '../../components/Main'
@@ -34,50 +35,60 @@ export default function Home({uniqueNumber, claimData}: HomeProps): ReactElement
         <title>{t('title')}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Header />
       <Main />
       <Footer />
-      {console.log("-Test Response-")} {/* testing */}
+      {console.log("-Test Response-")} {/* testing, to be removed before launch */}
       {console.dir({claimData})} {/* testing */}
     </Container>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, locale }) => {
-
+  const PASSWORD = process.env.CERTIFICATE_PASSPHRASE
   const API_URL: string = process.env.API_URL || "fail gracefully" //TODO: Fail gracefully. 
-  const UNIQUE_NUMBER_HEADER: string = process.env.UNIQUE_NUMBER_HEADER_TITLE || "x-unique-number"
-  const CERT_DIR: (string) = (process.env.CERTIFICATE_DIR || "/var/ssl/certs")
-  const CERT_PATH: string = path.join(CERT_DIR, '/ct.crt')
-  const PUB_KEY_PATH: string = path.join(CERT_DIR, "/ct-pub.key")
-  const KEY_PATH: string = path.join(CERT_DIR, "/ct.key")
+  // const UNIQUE_NUMBER_HEADER: string = process.env.UNIQUE_NUMBER_HEADER_TITLE || "x-unique-number"
+  const CERT_DIR: (string) = (process.env.CERTIFICATE_DIR || "/var/ssl")
+  const KEY_PATH: string = path.join(CERT_DIR, '/ct.p12')
 
-  let apiData: (string | null) = null;
+  let apiData: (string | null) = null
 
-  const headers = {
-    Accept: 'application/json'
-  };
+  // return certificate object with cert and key fields. 
+  async function getCertificate() {
+    const pemReadPkcs12 = promisify(pem.readPkcs12)
+    const pfx = fs.readFileSync(KEY_PATH);
+
+    const cert = await pemReadPkcs12(pfx, { p12Password: PASSWORD })
+
+    return cert;
+  }
+  
+  // TODO: Acquire uniqueNumber from header and use it to build API string 
+  // that will eventually be passed to makeRequest. 
+  // TODO: if no uniqueNumber, redirect. 
+  // const requestHeaders: IncomingHttpHeaders  = req.headers;
+  // const uniqueNumber = requestHeaders[UNIQUE_NUMBER_HEADER];
+  // function buildApiUrl(uniqueNumber, USER_KEY){}
+
+  async function makeRequest(certificate) {
+    const headers = {
+      Accept: 'application/json'
+    };
+
+    /* https://nodejs.org/api/tls.html#tls_tls_createsecurecontext_options
     
-  const requestHeaders: IncomingHttpHeaders  = req.headers;
-  const uniqueNumber = requestHeaders[UNIQUE_NUMBER_HEADER];
-  // TODO: if no uniqueNumber, redirect
-
-  async function makeRequest() {
-    // https://nodejs.org/api/tls.html#tls_tls_createsecurecontext_options
     // TODO: Store certs in memory, instead of loading every time. 
+    // TODO: rejectUnauthorized - set to true or implement a `checkServerIdentity` 
+    // function to check that the certificate is actually
+    //  issued by the host you're connecting to.
+    //  https://nodejs.org/api/https.html#https_https_request_url_options_callback
+    */
     const options = {
-      cert: fs.readFileSync(CERT_PATH, 'utf-8'),
-      key: fs.readFileSync(KEY_PATH, 'utf-8'),
-      // TODO: rejectUnauthorized - set to true or implement a `checkServerIdentity` 
-      // function to check that the certificate is actually
-      //  issued by the host you're connecting to.
-      //  https://nodejs.org/api/https.html#https_https_request_url_options_callback
+      cert: certificate.cert,
+      key: certificate.key,
       rejectUnauthorized: false, 
       keepAlive: false
     };
-  
-    // we're creating a new Agent that will now use the certs we have configured
     const sslConfiguredAgent = new https.Agent(options);
   
     try {
@@ -89,18 +100,20 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locale }) =>
       const responseBody = await response.json();
 
       apiData = responseBody
+
     } catch (error) {
       console.log(error);
     }
 
     return apiData;
   }
-  
-  const data = await makeRequest();
+
+  // where it comes together
+  const certificate = await getCertificate()
+  const data = await makeRequest(certificate)
 
   return ({
     props: {
-      uniqueNumber: uniqueNumber || null,
       claimData: [data],
       ...(await serverSideTranslations(locale || 'en', ['common', 'header', 'footer'])),
     }
