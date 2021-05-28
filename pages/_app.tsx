7ba14b45320692a3a405ed293fb11c855b687964
Kevin Moore <k.james.moore@gmail.com>
2021-05-28 08:54:57 -0400

    Only load .env file when doing local development.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,12 @@ import { appWithTranslation } from 'next-i18next'
 import { config } from 'dotenv'
 import { resolve } from 'path'
 
-// Loads the .env environmental variables into global process.env object.
-config({path: resolve(process.cwd(), '..','.env')})
+// Loads .env for local development.
+if (process.env.NODE_ENV === 'development') {
+  config({ path: resolve(process.cwd(), '..', '.env') })
+}
+
+console.log('ENV ----- ', process.env.NODE_ENV === 'production')
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
