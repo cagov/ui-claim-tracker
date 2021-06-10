f3d934aaa380697027b8f308ef6cffa6585396f4
Kat Tipton <kat@navapbc.com>
2021-06-10 09:39:41 -0400

    Adds loading test

--- tests/components/main.test.tsx
+++ tests/components/main.test.tsx
@@ -15,10 +15,25 @@ describe('Main component shows the page', () => {
     }
     ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
 
-    render(<Main />)
+    render(<Main loading={false} />)
     expect(screen.queryByText('UI Online')).toBeInTheDocument()
     expect(screen.queryByText('Benefit Year')).toBeInTheDocument()
     expect(screen.queryByText('Claim Status')).toBeInTheDocument()
+  })
+})
+
+describe('Main component shows loading', () => {
+  it('has our title, details, and body with shimmer', () => {
+    const mockRouter = {
+      locale: 'en',
+    }
+    ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
+
+    render(<Main loading />)
+    expect(screen.queryByText('UI Online')).toBeInTheDocument()
+    expect(screen.queryByText('Benefit Year')).not.toBeInTheDocument()
+    expect(screen.queryByText('Claim Status')).toBeInTheDocument()
+    expect(screen.queryByText('Your payments')).not.toBeInTheDocument()
   })
 })
 
@@ -29,7 +44,7 @@ describe('Main component shows the timeout', () => {
     }
     ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
 
-    render(<Main timedOut />)
+    render(<Main timedOut loading={false} />)
     expect(screen.queryByText('Your Session Will End Soon')).toBeInTheDocument()
   })
 })
