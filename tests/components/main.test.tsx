c506d5f130222830e89df737591186fdb87e891c
Kat Tipton <kat@navapbc.com>
2021-06-10 09:59:51 -0400

    Removes breadcrumb styling, locales, and fixes tests

--- tests/components/main.test.tsx
+++ tests/components/main.test.tsx
@@ -16,21 +16,21 @@ describe('Main component shows the page', () => {
     ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
 
     render(<Main loading={false} />)
-    expect(screen.queryByText('UI Online')).toBeInTheDocument()
+    expect(screen.queryByText('Claim Tracker')).toBeInTheDocument()
     expect(screen.queryByText('Benefit Year')).toBeInTheDocument()
     expect(screen.queryByText('Claim Status')).toBeInTheDocument()
   })
 })
 
 describe('Main component shows loading', () => {
-  it('has our title, details, and body with shimmer', () => {
+  it('has our titles but content is shimmer', () => {
     const mockRouter = {
       locale: 'en',
     }
     ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
 
     render(<Main loading />)
-    expect(screen.queryByText('UI Online')).toBeInTheDocument()
+    expect(screen.queryByText('Claim Tracker')).toBeInTheDocument()
     expect(screen.queryByText('Benefit Year')).not.toBeInTheDocument()
     expect(screen.queryByText('Claim Status')).toBeInTheDocument()
     expect(screen.queryByText('Your payments')).not.toBeInTheDocument()
