70463989fa67f574c2c624fc0fcc62fc9e26eb0b
Rocket ! <rocketnova@users.noreply.github.com>
2021-06-24 10:54:39 -0700

    Group getScenario() tests more clearly.

--- tests/utils/getScenarioContent.test.tsx
+++ tests/utils/getScenarioContent.test.tsx
@@ -1,7 +1,7 @@
 import getScenarioContent, { getScenario, ScenarioType } from '../../utils/getScenarioContent'
 import { ScenarioContent } from '../../types/common'
 
-// Shared test constants
+// Shared test constants for mock API gateway responses
 const pendingDeterminationScenario = { pendingDetermination: ['temporary text'] }
 const basePendingScenario = { hasPendingWeeks: true }
 const baseNoPendingScenario = { hasPendingWeeks: false }
@@ -24,14 +24,14 @@ describe('Retrieving the scenario content', () => {
   })
 })
 
-// Test getScenario()
-describe('Identifying the scenario', () => {
-  it('returns the pending determination scenario if there is a pendingDetermination object', () => {
+// Test getScenario(): pending determination scenario
+describe('The pending determination scenario', () => {
+  it('is returned when there is a pendingDetermination object', () => {
     const scenarioType: ScenarioType = getScenario(pendingDeterminationScenario)
     expect(scenarioType).toBe(ScenarioType.PendingDetermination)
   })
 
-  it('returns the pending determination scenario if there is a pendingDetermination object regardless of other criteria', () => {
+  it('is returned when there is a pendingDetermination object regardless of other criteria', () => {
     const pendingDeterminationScenarioWith = {
       pendingDetermination: ['temporary text'],
       hasPendingWeeks: true,
@@ -46,36 +46,42 @@ describe('Identifying the scenario', () => {
     const scenarioTypeWithout: ScenarioType = getScenario(pendingDeterminationScenarioWithout)
     expect(scenarioTypeWithout).toBe(ScenarioType.PendingDetermination)
   })
+})
 
-  it('returns the base state (with pending weeks) if there are pending weeks', () => {
+// Test getScenario(): base state with pending weeks scenario
+describe('The base state (with pending weeks) scenario', () => {
+  it('is returned when there are pending weeks', () => {
     const scenarioType: ScenarioType = getScenario(basePendingScenario)
     expect(scenarioType).toBe(ScenarioType.BasePending)
   })
 
-  it('returns the base state (with pending weeks) if there are pending weeks and pendingDetermination is null', () => {
+  it('is returned when there are pending weeks and pendingDetermination is null', () => {
     const basePendingScenarioNull = { hasPendingWeeks: true, pendingDetermination: null }
     const scenarioType: ScenarioType = getScenario(basePendingScenarioNull)
     expect(scenarioType).toBe(ScenarioType.BasePending)
   })
 
-  it('returns the base state (with pending weeks) if there are pending weeks and pendingDetermination is an empty array', () => {
+  it('is returned when there are pending weeks and pendingDetermination is an empty array', () => {
     const basePendingScenarioEmpty = { hasPendingWeeks: true, pendingDetermination: [] }
     const scenarioType: ScenarioType = getScenario(basePendingScenarioEmpty)
     expect(scenarioType).toBe(ScenarioType.BasePending)
   })
+})
 
-  it('returns base state (with no pending weeks) if there are no pending weeks', () => {
+// Test getScenario(): base state with no pending weeks scenario
+describe('The base state (with no pending weeks) scenario', () => {
+  it('is returned when there are no pending weeks', () => {
     const scenarioType: ScenarioType = getScenario(baseNoPendingScenario)
     expect(scenarioType).toBe(ScenarioType.BaseNoPending)
   })
 
-  it('returns base state (with no pending weeks) if there are no pending weeks and pendingDetermination is null', () => {
+  it('is returned when there are no pending weeks and pendingDetermination is null', () => {
     const baseNoPendingScenarioNull = { hasPendingWeeks: false, pendingDetermination: null }
     const scenarioType: ScenarioType = getScenario(baseNoPendingScenarioNull)
     expect(scenarioType).toBe(ScenarioType.BaseNoPending)
   })
 
-  it('returns base state (with no pending weeks) if there are no pending weeks and pendingDetermination is an empty array', () => {
+  it('is returned when there are no pending weeks and pendingDetermination is an empty array', () => {
     const baseNoPendingScenarioEmpty = { hasPendingWeeks: false, pendingDetermination: [] }
     const scenarioType: ScenarioType = getScenario(baseNoPendingScenarioEmpty)
     expect(scenarioType).toBe(ScenarioType.BaseNoPending)
