e0ca6d233fd56549f86c21766192306050bebb91
Rocket ! <rocketnova@users.noreply.github.com>
2021-06-24 16:42:43 -0700

    Add reusable wrapper component around <Trans>.

---
+++ components/TransLine.tsx
@@ -0,0 +1,21 @@
+import { Trans } from 'react-i18next'
+import React from 'react'
+
+export interface TransLineProps {
+  i18nString: string // Expects an i18n string
+  links?: string[] | null
+}
+
+export const TransLine: React.FC<TransLineProps> = ({ i18nString, links = null }) => {
+  let linkComponents: JSX.Element[] = []
+  if (links && links.length > 0) {
+    // Disabling some linting rules for this line. The anchor <a> element will
+    // be interporlated by <Trans>.
+    /* eslint-disable jsx-a11y/anchor-has-content */
+    /* eslint-disable react/self-closing-comp */
+    linkComponents = links.map((link, index) => <a href={link} key={index}></a>)
+    /* eslint-enable jsx-a11y/anchor-has-content */
+    /* eslint-enable react/self-closing-comp */
+  }
+  return <Trans i18nKey={i18nString}>{linkComponents}</Trans>
+}
---
+++ stories/TransLine.stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react'
+import { TransLine as TransLineComponent, TransLineProps } from '../components/TransLine'
+
+export default {
+  title: 'Component/Atoms/Trans Line',
+  component: TransLineComponent,
+} as Meta
+
+const Template: Story<TransLineProps> = (args) => <TransLineComponent {...args} />
+
+export const TransLine = Template.bind({})
+TransLine.args = {
+  i18nString: 'work-in-progress.message',
+  links: ['https://navapbc.com', 'https://edd.ca.gov'],
+}
