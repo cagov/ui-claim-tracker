77d37a37d95fb6c70674853290de08ca8e469c0a
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-07 16:10:39 -0700

    Add more to pino-applicationinsights typing.

--- types/pino-applicationinsights/index.d.ts
+++ types/pino-applicationinsights/index.d.ts
@@ -1,9 +1,11 @@
 declare module 'pino-applicationinsights' {
   import Pumpify from 'pumpify'
+  import ApplicationInsights from 'appliactioninsights'
 
   interface WriteStreamOptions {
     key?: string
+    setup?: (applicationInsights: ApplicationInsights) => void
   }
 
-  function createWriteStream(options: WriteStreamOptions): Promise<Pumpify>
+  function createWriteStream(options?: WriteStreamOptions): Promise<Pumpify>
 }
