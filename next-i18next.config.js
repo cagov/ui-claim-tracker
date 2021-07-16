803a46d78b706c55d30703ff60b95fdd0d9c299d
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-15 19:00:42 -0700

    Add support for <em> in Trans components.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,10 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     react: {
-      useSuspense: false, //Explicitly set this to false to avoid conflicts with our i18n system
-    }
+      useSuspense: false, //Explicitly set this to false to avoid conflicts with our i18n system,
+      // Add support for <em>.
+      // See https://react.i18next.com/latest/trans-component#using-for-less-than-br-greater-than-and-other-simple-html-elements-in-translations-v-10-4-0
+      transKeepBasicHtmlNodesFor: ['br', 'strong', 'i', 'p', 'em'],
+    },
   },
 }
