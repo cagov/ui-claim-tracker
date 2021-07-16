ee341626c30329011d4faa97fde9a644a9624100
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-15 18:04:29 -0700

    Clean up WIP.

--- utils/getClaimStatus.tsx
+++ utils/getClaimStatus.tsx
@@ -7,23 +7,13 @@ import { ClaimStatusContent, I18nString, TextOptionalLink, TransLineProps } from
 import { ScenarioType } from './getScenarioContent'
 import getUrl, { UrlType } from './getUrl'
 
-type ClaimStatusJson = WrapperJson | ScenarioWrapperJson | ScenarioJson | string | TextOptionalLink | TextOptionalLink[]
-
 type StepType = 'your-next-steps' | 'edd-next-steps'
 
-interface ScenarioJson {
+interface ClaimStatusScenarioJson {
   heading: string
   summary: TextOptionalLink
   'your-next-steps': TextOptionalLink[]
   'edd-next-steps': TextOptionalLink[]
-}
-
-interface ScenarioWrapperJson {
-  [key: string]: ScenarioJson
-}
-
-interface WrapperJson {
-  scenarios: ScenarioWrapperJson
 }
 
 /**
@@ -36,21 +26,21 @@ export function scenarioToString(scenarioType: ScenarioType): string {
 /**
  * Helper function to get shared Claim Status translation string prefix.
  */
-function getTranslationPrefix(scenarioType: ScenarioType): I18nString {
+function buildTranslationPrefix(scenarioType: ScenarioType): I18nString {
   return `claim-status:scenarios.${scenarioToString(scenarioType)}`
 }
 
 /**
  * Get Claim Status heading.
  */
-export function getClaimStatusHeading(scenarioType: ScenarioType): I18nString {
-  return getTranslationPrefix(scenarioType) + '.heading'
+export function buildClaimStatusHeading(scenarioType: ScenarioType): I18nString {
+  return buildTranslationPrefix(scenarioType) + '.heading'
 }
 
 /**
  * Build a list of urls from keys in the json translation files.
  */
-export function getTransLineLinks(linkKeys: string[] | undefined): string[] {
+export function buildTransLineLinks(linkKeys: string[] | undefined): string[] {
   const links: string[] = []
   if (linkKeys) {
     if (linkKeys && linkKeys.length > 0) {
@@ -70,30 +60,16 @@ export function getTransLineLinks(linkKeys: string[] | undefined): string[] {
 /**
  * Build props to pass to the TransLine react component.
  */
-export function getTransLineProps(targetJson: TextOptionalLink, i18nKey: string): TransLineProps {
-  // jsonObject could possibly be null.
-  // if (!jsonObject) {
-  //   throw new Error('Unable to retrieve JSON object')
-  // }
-
-  // // Get the links.
-  // let links: string[] = []
-  // if (textObject.links) {
-  //   links = getTransLineLinks(targetJson.links as string[])
-  // }
-  // const linksJson = getChildJson(targetJson, ['links'], 0)
-  const links = getTransLineLinks(targetJson.links)
-
-  // Append to the keys array to build the full i18nKey.
-  // keys.push('text')
-
-  // Return the final TransLineProps object.
+export function buildTransLineProps(json: TextOptionalLink, i18nKey: I18nString): TransLineProps {
   return {
     i18nKey: i18nKey,
-    links: links,
+    links: buildTransLineLinks(json.links),
   }
 }
 
+/**
+ * Helper function to construct the i18nKey for claim status link text.
+ */
 function buildI18nKey(keys: string[]): I18nString {
   return 'claim-status:' + keys.join('.') + '.text'
 }
@@ -101,27 +77,27 @@ function buildI18nKey(keys: string[]): I18nString {
 /**
  * Get Claim Status summary.
  */
-export function getClaimStatusSummary(scenarioObject: ScenarioJson, scenarioString: string): TransLineProps {
+export function buildClaimStatusSummary(
+  scenarioObject: ClaimStatusScenarioJson,
+  scenarioString: string,
+): TransLineProps {
   const keys = ['scenarios', scenarioString, 'summary']
-  // const jsonObject = getChildJson(scenarioObject, ['summary'])
-  return getTransLineProps(scenarioObject.summary, buildI18nKey(keys))
+  return buildTransLineProps(scenarioObject.summary, buildI18nKey(keys))
 }
 
 /**
  * Get next steps content for Claim Status.
  */
-export function getNextSteps(
-  scenarioObject: ScenarioJson,
+export function buildNextSteps(
+  scenarioObject: ClaimStatusScenarioJson,
   scenarioString: string,
   whichSteps: StepType,
 ): TransLineProps[] {
   const steps: TransLineProps[] = []
-
-  // const targetJson = getChildJson(scenarioObject, [whichSteps])
-  const targetJson = scenarioObject[whichSteps]
-  for (const [index, value] of targetJson.entries()) {
+  const json = scenarioObject[whichSteps]
+  for (const [index, value] of json.entries()) {
     const keys = ['scenarios', scenarioString, whichSteps, index.toString()]
-    steps.push(getTransLineProps(value, buildI18nKey(keys)))
+    steps.push(buildTransLineProps(value, buildI18nKey(keys)))
   }
   return steps
 }
@@ -130,16 +106,15 @@ export function getNextSteps(
  * Get combined Claim Status content.
  */
 export default function getClaimStatus(scenarioType: ScenarioType): ClaimStatusContent {
+  // Explicitly cast the scenario string (e.g. scenario1, scenario2) into the union of literal types
+  // expected by Typescript.
   const scenarioString = scenarioToString(scenarioType) as keyof typeof claimStatusJson.scenarios
-  // const scenarioObject = getChildJson(claimStatusJson, ['scenarios', scenarioString])
+
   const scenarioObject = claimStatusJson.scenarios[scenarioString]
 
-  const statusContent: ClaimStatusContent = {
-    heading: getClaimStatusHeading(scenarioType),
-    summary: getClaimStatusSummary(scenarioObject, scenarioString),
-    yourNextSteps: getNextSteps(scenarioObject, scenarioString, 'your-next-steps'),
-    // yourNextSteps: [{ i18nKey: '' }],
+  return {
+    heading: buildClaimStatusHeading(scenarioType),
+    summary: buildClaimStatusSummary(scenarioObject, scenarioString),
+    yourNextSteps: buildNextSteps(scenarioObject, scenarioString, 'your-next-steps'),
   }
-
-  return statusContent
 }
