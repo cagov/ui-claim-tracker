ba0009bad22c77065b7a6390220b0a4a4e154a2d
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-21 11:43:21 -0700

    Add isValidDate().

--- tests/utils/formatDate.test.ts
+++ tests/utils/formatDate.test.ts
@@ -1,4 +1,19 @@
-import formatDate from '../../utils/formatDate'
+import formatDate, { isValidDate } from '../../utils/formatDate'
+
+// Test isValidDate()
+describe('A date is', () => {
+  it('invalid if it is not a date', () => {
+    expect(isValidDate('nonsense')).toBe(false)
+  })
+
+  it('invalid if it is earlier than the minimum date', () => {
+    expect(isValidDate('0001-01-01T00:00:00')).toBe(false)
+  })
+
+  it('valid if it is later than the minimum date', () => {
+    expect(isValidDate('2013-01-01T00:00:00')).toBe(true)
+  })
+})
 
 // Test formatDate()
 describe('Formatting dates', () => {
--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,4 +1,4 @@
-import { format, parse } from 'date-fns'
+import { format, isValid, parse } from 'date-fns'
 
 /**
  * Parse dates formatted by the API gateway.
@@ -6,6 +6,33 @@ import { format, parse } from 'date-fns'
 export function parseApiDate(dateString: string): Date {
   return parse(dateString, "yyyy-MM-dd'T'HH:mm:ss", new Date())
 }
+
+/**
+ * Determine if the date string is valid.
+ */
+export function isValidDate(dateString: string): boolean {
+  const date = parseApiDate(dateString)
+  if (isValid(date)) {
+    // Set a min date because it's possible for the date
+    // to be '0001-01-01'.
+    const minDate = new Date('1900-01-01')
+    if (date <= minDate) {
+      return false
+    }
+    // date-fns says the date is valid
+    // AND the date is sooner than the min date.
+    else {
+      return true
+    }
+  }
+  // date-fns says the date is invalid
+  else {
+    return false
+  }
+}
+
+// @TODO: add a function to check and log any dates that are earlier
+// than 2020 as these are anomolous dates that could indicate an error.
 
 /**
  * Format dates for user-facing display.
