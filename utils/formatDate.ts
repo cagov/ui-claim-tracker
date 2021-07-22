273edb37214443988d4d8a16842518515623c679
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-22 12:17:51 -0700

    Explicitly assume all datetimes that are missing timezones are in Pacific Time.

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,21 +1,30 @@
+/**
+ * Utility file to handle datetimes.
+ *
+ * Because this is a State of California application, we assume all times are
+ * in Pacific Time if there is no timezone provided in the datetime string.
+ */
 import { format, isValid, parse } from 'date-fns'
+import { zonedTimeToUtc } from 'date-fns-tz'
+
+const pacificTimeZone = 'America/Los_Angeles'
 
 /**
- * Parse dates formatted by the API gateway.
+ * Assume input is in Pacific Time and convert to UTC.
  */
-export function parseApiDate(dateString: string): Date {
-  return parse(dateString, "yyyy-MM-dd'T'HH:mm:ss", new Date())
+export function datetimeInUtc(date: string | Date): Date {
+  return zonedTimeToUtc(date, pacificTimeZone)
 }
 
 /**
  * Determine if the date string is valid.
  */
 export function isValidDate(dateString: string): boolean {
-  const date = parseApiDate(dateString)
+  const date = datetimeInUtc(dateString)
   if (isValid(date)) {
     // Set a min date because it's possible for the date
     // to be '0001-01-01'.
-    const minDate = new Date('1900-01-01')
+    const minDate = datetimeInUtc('1900-01-01')
     if (date <= minDate) {
       return false
     }
@@ -38,6 +47,6 @@ export function isValidDate(dateString: string): boolean {
  * Format dates for user-facing display.
  */
 export default function formatDate(dateString: string): string {
-  const parsedDate = parseApiDate(dateString)
+  const parsedDate = datetimeInUtc(dateString)
   return format(parsedDate, 'M/d/yyyy')
 }
