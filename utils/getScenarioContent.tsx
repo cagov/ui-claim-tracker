a5707075066e9eaff29749926f925095b932008b
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-22 15:59:07 -0700

    Rough out pending determination logic.

--- utils/getScenarioContent.tsx
+++ utils/getScenarioContent.tsx
@@ -10,19 +10,29 @@
 import { Claim, ClaimDetailsContent, PendingDetermination, ScenarioContent } from '../types/common'
 import getClaimDetails from './getClaimDetails'
 import getClaimStatus from './getClaimStatus'
+import { datetimeInUtc, isDatePast } from './formatDate'
 
 export enum ScenarioType {
   Scenario1,
+  Scenario2,
+  Scenario3,
   Scenario4,
   Scenario5,
   Scenario6,
 }
 
 export const ScenarioTypeNames = {
-  [ScenarioType.Scenario1]: 'Pending determination scenario',
+  [ScenarioType.Scenario1]: 'Determination interview: not yet scheduled',
+  [ScenarioType.Scenario2]: 'Determination interview: scheduled',
+  [ScenarioType.Scenario3]: 'Determination interview: awaiting decision',
   [ScenarioType.Scenario4]: 'Generic pending state: pending weeks',
   [ScenarioType.Scenario5]: 'Base state: no pending weeks, no weeks to certify',
   [ScenarioType.Scenario6]: 'Base state: no pending weeks, weeks to certify',
+}
+
+interface PendingDeterminationScenario {
+  scenarioType: ScenarioType
+  pendingDetermination?: PendingDetermination
 }
 
 export const NonPendingDeterminationValues = ['Canceled', 'Complete', 'TRAN', 'INVL', 'IDNC', '1277', 'OTHR', 'ClmCX']
@@ -42,33 +52,109 @@ export function isDeterminationStatusPending(pendingDetermination: PendingDeterm
   )
 }
 
+export function isStrictlyBefore(first: PendingDetermination, second: PendingDetermination): boolean {
+  return true
+}
+
+/**
+ * Identify whether the scenario is one of the pending determination scenarios.
+ */
+export function identifyPendingDeterminationScenario(
+  pendingDeterminations: PendingDetermination[],
+): PendingDeterminationScenario | undefined {
+  let earliestScheduled: PendingDetermination | undefined
+
+  // Track whether any of the pendingDetermination objects meet the other scenario criteria.
+  let hasAwaitingDecision = false
+  let hasNotYetScheduled = false
+
+  // Loop through all the pendingDetermination objects.
+  for (const pendingDetermination of pendingDeterminations) {
+    if (isDeterminationStatusPending(pendingDetermination)) {
+      // Scenario 2:
+      // If Determination Status is Pending
+      // AND Schedule Date is today or in the future
+      if (!isDatePast(datetimeInUtc(pendingDetermination.scheduleDate))) {
+        // If we haven't found a pendingDetermination object that is scheduled yet
+        // OR the current pendingDetermination object is earlier than the previous one found
+        // Then update the earliest found scheduled pendingDetermination object
+        if (!earliestScheduled || isStrictlyBefore(pendingDetermination, earliestScheduled)) {
+          earliestScheduled = pendingDetermination
+        }
+      }
+      // Scenario 3:
+      // If Determination Status is Pending
+      // AND Schedule Date is in the past
+      else {
+        hasAwaitingDecision = true
+      }
+    }
+    // Scenario 1:
+    // If determinationStatus is empty/null/undefined/unset
+    // AND scheduleDate has no value
+    // AND requestDate has a value
+    else if (
+      !pendingDetermination.determinationStatus &&
+      !pendingDetermination.scheduleDate &&
+      pendingDetermination.requestDate
+    ) {
+      hasNotYetScheduled = true
+    }
+    // All other combinations are invalid!
+  }
+
+  // Scenarios 2 takes priority over Scenarios 1 & 3.
+  if (earliestScheduled) {
+    return {
+      scenarioType: ScenarioType.Scenario2,
+      pendingDetermination: earliestScheduled,
+    }
+  } else {
+    // Scenario 3 takes priority over Scenario 1.
+    if (hasAwaitingDecision) {
+      return { scenarioType: ScenarioType.Scenario3 }
+    } else if (hasNotYetScheduled) {
+      return { scenarioType: ScenarioType.Scenario1 }
+    }
+    // Otherwise, no valid pendingDetermination objects; return undefined.
+    else {
+      return undefined
+    }
+  }
+}
+
 /**
  * Identify the correct scenario to display.
  *
  * @TODO: Validating the API gateway response #150
  */
 export function getScenario(claimData: Claim): ScenarioType {
-  // The pending determination scenario: if claimData contains any pendingDetermination
-  // objects
-  // @TODO: refactor with more detailed pending determination scenarios #252
+  // If there are any pendingDetermination objects, the scenario MIGHT be one of the
+  // pending determination scenarios.
   if (claimData.pendingDetermination && claimData.pendingDetermination.length > 0) {
-    return ScenarioType.Scenario1
+    const pendingDeterminationScenario = identifyPendingDeterminationScenario(claimData.pendingDetermination)
+
+    // It's possible to have pending determination objects, but still not be a valid
+    // pending determination scenario, so check to see if the returned object is undefined.
+    if (pendingDeterminationScenario) {
+      return pendingDeterminationScenario.scenarioType
+    }
   }
 
-  // No pendingDetermination objects.
+  // If the scenario is not one of the Pending Determination scenarios,
+  // check to see if it one of the remaining scenarios.
+
+  // @TODO: Validate that hasPendingWeeks is a boolean
+  if (claimData.hasPendingWeeks === true) {
+    // @TODO: Validate that hasCertificationWeeks is a boolean
+    return ScenarioType.Scenario4
+  }
+  // hasPendingWeeks === false
   else {
-    // @TODO: Validate that hasPendingWeeks is a boolean
-    if (claimData.hasPendingWeeks === true) {
-      // @TODO: Validate that hasCertificationWeeks is a boolean
-      return ScenarioType.Scenario4
-    }
-    // hasPendingWeeks === false
-    else {
-      if (claimData.hasCertificationWeeksAvailable === false) {
-        return ScenarioType.Scenario5
-      } else {
-        return ScenarioType.Scenario6
-      }
+    if (claimData.hasCertificationWeeksAvailable === false) {
+      return ScenarioType.Scenario5
+    } else {
+      return ScenarioType.Scenario6
     }
   }
 }
