36d9e4ff2eebfa5f03dfaffa05a21c016416e2bf
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-22 15:17:33 -0700

    Refactor TransLine to have a separate function.

--- components/TransLine.tsx
+++ components/TransLine.tsx
@@ -1,12 +1,31 @@
 import { Trans, useTranslation } from 'react-i18next'
 import React from 'react'
 import { Shimmer } from './Shimmer'
-import { TransLineContent } from '../types/common'
+import { I18nString, TransLineContent } from '../types/common'
 import getUrl from '../utils/getUrl'
 
 export interface TransLineProps extends TransLineContent {
   loading: boolean
   userArrivedFromUioMobile: boolean
+}
+
+/**
+ * Handle url resolution.
+ */
+function resolveUrl(link: I18nString, userArrivedFromUioMobile: boolean) {
+  // Special case for UIO homepage links.
+  if (link === 'uio-home') {
+    const uioHomeLink = userArrivedFromUioMobile ? getUrl('uio-home-url-mobile') : getUrl('uio-home-url-desktop')
+    if (uioHomeLink) {
+      // If the link is for UIO homepage, do a direct getUrl() lookup.
+      // Do not pass the looked up url through t() because t() will mangle the url.
+      return uioHomeLink
+    }
+  }
+
+  // Otherwise, use t() to lookup the correct language-dependent url.
+  const { t } = useTranslation(['common', 'claim-details', 'claim-status'])
+  return t(link)
 }
 
 export const TransLine: React.FC<TransLineProps> = ({
@@ -19,25 +38,10 @@ export const TransLine: React.FC<TransLineProps> = ({
     return <Shimmer width={120} height={15} baseColor="#B6B2B2" shimColor="#656565" borderRadius={3} />
   }
 
-  const { t } = useTranslation(['common', 'claim-details', 'claim-status'])
-
   const linkComponents: JSX.Element[] = []
   if (links && links.length > 0) {
     for (const link of links) {
-      // Special case for UIO homepage links.
-      let href = ''
-      if (link === 'uio-home') {
-        const uioHomeLink = userArrivedFromUioMobile ? getUrl('uio-home-url-mobile') : getUrl('uio-home-url-desktop')
-        if (uioHomeLink) {
-          // If the link is for UIO homepage, do a direct getUrl() lookup.
-          // Do not pass the looked up url through t() because t() will mangle the url.
-          href = uioHomeLink
-        }
-      }
-      // Otherwise, use t() to lookup the correct language-dependent url.
-      else {
-        href = t(link)
-      }
+      const href = resolveUrl(link, userArrivedFromUioMobile)
       // Disabling some linting rules for this line. The anchor <a> element will
       // be interpolated by <Trans>.
       /* eslint-disable jsx-a11y/anchor-has-content */
