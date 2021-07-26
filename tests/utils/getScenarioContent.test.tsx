8211e7e200283b2413860b754aae9a7c92fe8542
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-26 15:48:05 -0700

    Add tests for pending determination scenarios.

--- tests/utils/getScenarioContent.test.tsx
+++ tests/utils/getScenarioContent.test.tsx
@@ -1,76 +1,25 @@
+import { format } from 'date-fns'
+
 import { PendingDetermination } from '../../types/common'
+import apiGatewayStub from '../../utils/apiGatewayStub'
 import {
   getScenario,
+  identifyPendingDeterminationScenario,
   isDeterminationStatusPending,
   isScheduledStrictlyBefore,
   NonPendingDeterminationValues,
   ScenarioType,
 } from '../../utils/getScenarioContent'
-import apiGatewayStub from '../../utils/apiGatewayStub'
+import { datetimeInUtc } from '../../utils/formatDate'
 
 /**
- * Test getScenario()
- *
- * Refer to ScenarioType and ScenarioTypeNames for which scenario has which number.
+ * Test helpers.
  */
+function formatFromApiGateway(date: Date): string {
+  return format(date, "yyyy-MM-dd'T'HH:mm:ss")
+}
 
-// Scenario 1
-describe('Scenario 1', () => {
-  it('is returned when there is a pendingDetermination object', () => {
-    const pendingDeterminationScenario = apiGatewayStub(ScenarioType.Scenario1)
-    const scenarioType = getScenario(pendingDeterminationScenario)
-    expect(scenarioType).toBe(ScenarioType.Scenario1)
-  })
-
-  it('is returned when there is a pendingDetermination object regardless of other criteria', () => {
-    const pendingDeterminationScenarioWith = {
-      pendingDetermination: ['temporary text'],
-      hasPendingWeeks: true,
-    }
-    const scenarioTypeWith = getScenario(pendingDeterminationScenarioWith)
-    expect(scenarioTypeWith).toBe(ScenarioType.Scenario1)
-
-    const pendingDeterminationScenarioWithout = {
-      pendingDetermination: ['temporary text'],
-      hasPendingWeeks: false,
-    }
-    const scenarioTypeWithout = getScenario(pendingDeterminationScenarioWithout)
-    expect(scenarioTypeWithout).toBe(ScenarioType.Scenario1)
-  })
-
-  it('is not returned if pendingDetermination is null', () => {
-    const pendingDeterminationScenarioNull = { pendingDetermination: null }
-    const scenarioTypeNull = getScenario(pendingDeterminationScenarioNull)
-    expect(scenarioTypeNull).not.toBe(ScenarioType.Scenario1)
-  })
-
-  it('is not returned if pendingDetermination is an empty array', () => {
-    const pendingDeterminationScenarioEmpty = { pendingDetermination: [] }
-    const scenarioTypeEmpty = getScenario(pendingDeterminationScenarioEmpty)
-    expect(scenarioTypeEmpty).not.toBe(ScenarioType.Scenario1)
-  })
-})
-
-// Scenario 4
-describe('The Generic Pending scenario', () => {
-  it('is returned as expected', () => {
-    const scenarioType = ScenarioType.Scenario4
-    expect(getScenario(apiGatewayStub(scenarioType))).toBe(scenarioType)
-  })
-})
-
-// Scenarios 5 & 6
-describe('The Base State scenarios', () => {
-  it('are returned as expected', () => {
-    const baseScenarios = [ScenarioType.Scenario5, ScenarioType.Scenario6]
-    for (const scenarioType of baseScenarios) {
-      expect(getScenario(apiGatewayStub(scenarioType))).toBe(scenarioType)
-    }
-  })
-})
-
-// Test isDeterminationStatusPending()
-describe('Determination Status', () => {
+function getMockPendingDetermination(): PendingDetermination {
   // Shared mock data.
   const pendingDetermination: PendingDetermination = {
     pendingDate: '',
@@ -82,33 +31,348 @@ describe('Determination Status', () => {
     spokenLanguageCode: '',
     spokenLanguageDesc: '',
   }
+  return pendingDetermination
+}
 
+/**
+ * Test getScenario()
+ *
+ * Refer to ScenarioType and ScenarioTypeNames for which scenario has which number.
+ */
+
+// Scenarios 1, 2, 3
+describe('Scenarios 1, 2, 3', () => {
+  it('are not returned if pendingDetermination is null', () => {
+    const pendingDeterminationScenarioNull = { pendingDetermination: null }
+    const scenarioTypeNull = getScenario(pendingDeterminationScenarioNull)
+    expect(scenarioTypeNull).not.toBe(ScenarioType.Scenario1)
+    expect(scenarioTypeNull).not.toBe(ScenarioType.Scenario2)
+    expect(scenarioTypeNull).not.toBe(ScenarioType.Scenario3)
+  })
+
+  it('are not returned if pendingDetermination is an empty array', () => {
+    const pendingDeterminationScenarioEmpty = { pendingDetermination: [] }
+    const scenarioTypeEmpty = getScenario(pendingDeterminationScenarioEmpty)
+    expect(scenarioTypeEmpty).not.toBe(ScenarioType.Scenario1)
+    expect(scenarioTypeEmpty).not.toBe(ScenarioType.Scenario2)
+    expect(scenarioTypeEmpty).not.toBe(ScenarioType.Scenario3)
+  })
+})
+
+// Scenario 4
+describe('The Generic Pending scenario (scenario 4)', () => {
+  it('is returned as expected', () => {
+    const scenarioType = ScenarioType.Scenario4
+    expect(getScenario(apiGatewayStub(scenarioType))).toBe(scenarioType)
+  })
+})
+
+// Scenarios 5 & 6
+describe('The Base State scenarios (scenarios 5 & 6)', () => {
+  it('are returned as expected', () => {
+    const baseScenarios = [ScenarioType.Scenario5, ScenarioType.Scenario6]
+    for (const scenarioType of baseScenarios) {
+      expect(getScenario(apiGatewayStub(scenarioType))).toBe(scenarioType)
+    }
+  })
+})
+
+/**
+ * Test Test identifyPendingDeterminationScenario()
+ */
+
+// Scenario 1: Test various values of pendingDetermination.determinationStatus
+describe('The determination interview is not yet scheduled (scenario 1) when all other criteria are met and the determination status evaluates to', () => {
+  it('false (null)', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = null
+    pendingDetermination.scheduleDate = null
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+
+  it('false (empty string)', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = ''
+    pendingDetermination.scheduleDate = null
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+
+  it('false (undefined)', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = undefined
+    pendingDetermination.scheduleDate = null
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+})
+
+// Scenario 1: Test various values of pendingDetermination.scheduleDate
+describe('The determination interview is not yet scheduled (scenario 1) when all other criteria are met and the schedule date evaluates to', () => {
+  it('false (null)', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = null
+    pendingDetermination.scheduleDate = null
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+
+  it('false (empty string)', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = null
+    pendingDetermination.scheduleDate = ''
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+
+  it('false (undefined)', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = null
+    pendingDetermination.scheduleDate = undefined
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+})
+
+// Test various Scenario 1 invalid values
+describe('The determination interview is invalid if all other Scenario 1 critera are met, but', () => {
+  it('the determination status does not evaluate to false', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = 'non-null value'
+    pendingDetermination.scheduleDate = ''
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result).toBe(null)
+  })
+
+  it('the schedule date does not evaluate to false', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    // Determination Status is one of NonPendingDeterminationValues values
+    pendingDetermination.determinationStatus = 'Complete'
+    pendingDetermination.scheduleDate = '2000-01-01T00:00:00'
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result).toBe(null)
+  })
+
+  it('the schedule date is an invalid datetime string', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = ''
+    pendingDetermination.scheduleDate = 'non-null value'
+    pendingDetermination.requestDate = 'any non-null value'
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result).toBe(null)
+  })
+
+  it('the request date evaluates to false', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = ''
+    pendingDetermination.scheduleDate = ''
+    pendingDetermination.requestDate = ''
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result).toBe(null)
+  })
+})
+
+// Scenario 2
+describe('The determination interview is scheduled (scenario 2)', () => {
+  it('when the determination status is pending and there is one pending determination object with a schedule date in the future', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+
+    const today = new Date()
+    const tomorrow = today.setDate(today.getDate() + 1)
+    const tomorrowUtc = datetimeInUtc(tomorrow)
+
+    pendingDetermination.scheduleDate = formatFromApiGateway(tomorrowUtc)
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario2)
+  })
+
+  it('scheduled (scenario 2) when the determination status is pending and there is one pending determination object with a schedule date of today', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+
+    const today = new Date()
+    const todayUtc = datetimeInUtc(today)
+
+    // Mock the result of Date.now() to today at midnight
+    Date.now = jest.fn(() => today.setUTCHours(0, 0, 0, 0))
+
+    pendingDetermination.scheduleDate = formatFromApiGateway(todayUtc)
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario2)
+  })
+})
+
+// Scenario 3
+describe('The determination interview is awating decision (scenario 3)', () => {
+  it('when the determination status is pending and there is one pending determination object with a schedule date in the past', () => {
+    const pendingDetermination = getMockPendingDetermination()
+    pendingDetermination.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+
+    const today = new Date()
+    const yesterday = today.setDate(today.getDate() - 1)
+    const yesterdayUtc = datetimeInUtc(yesterday)
+
+    pendingDetermination.scheduleDate = formatFromApiGateway(yesterdayUtc)
+    const result = identifyPendingDeterminationScenario([pendingDetermination])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario3)
+  })
+})
+
+// Test multiple pendingDetermination objects.
+describe('When there are multiple pendingDetermination objects, the determination interview is', () => {
+  // Multiple "not yet scheduled" pendingDetermination objects that evaluate
+  // to Scenario 1, return Scenario 1.
+  it('not yet scheduled (scenario 1) if all are not yet scheduled', () => {
+    const appt1 = getMockPendingDetermination()
+    appt1.pendingDetermination = ''
+    appt1.scheduleDate = ''
+    appt1.requestDate = 'not empty'
+
+    const appt2 = getMockPendingDetermination()
+    appt2.pendingDetermination = ''
+    appt2.scheduleDate = ''
+    appt2.requestDate = 'not empty'
+
+    const result = identifyPendingDeterminationScenario([appt1, appt2])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario1)
+  })
+
+  // Multiple "scheduled" pendingDetermination objects that evaluate
+  // to Scenario 2, returns the object with the earliest schedule date
+  it('scheduled (scenario 2) if all are scheduled', () => {
+    const appt1 = getMockPendingDetermination()
+    appt1.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const now1 = new Date()
+    const tomorrow = now1.setDate(now1.getDate() + 1)
+    const tomorrowUtc = datetimeInUtc(tomorrow)
+    appt1.scheduleDate = formatFromApiGateway(tomorrowUtc)
+
+    const appt2 = getMockPendingDetermination()
+    appt2.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const now2 = new Date()
+    const aWeekFromNow = now2.setDate(now2.getDate() + 7)
+    const aWeekFromNowUtc = datetimeInUtc(aWeekFromNow)
+    appt2.scheduleDate = formatFromApiGateway(aWeekFromNowUtc)
+
+    const result = identifyPendingDeterminationScenario([appt1, appt2])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario2)
+    expect(result.pendingDetermination).toBe(appt1)
+  })
+
+  // Multiple "awaiting decision" pendingDetermination objects that evaluate
+  // to Scenario 3, return Scenario 3.
+  it('awaiting decision (scenario 3) if all are awaiting decision', () => {
+    const appt1 = getMockPendingDetermination()
+    appt1.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const now1 = new Date()
+    const yesterday = now1.setDate(now1.getDate() - 1)
+    const yesterdayUtc = datetimeInUtc(yesterday)
+    appt1.scheduleDate = formatFromApiGateway(yesterdayUtc)
+
+    const appt2 = getMockPendingDetermination()
+    appt2.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const now2 = new Date()
+    const aWeekAgo = now2.setDate(now2.getDate() - 7)
+    const aWeekAgoUtc = datetimeInUtc(aWeekAgo)
+    appt2.scheduleDate = formatFromApiGateway(aWeekAgoUtc)
+
+    const result = identifyPendingDeterminationScenario([appt1, appt2])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario3)
+  })
+
+  // If all three types of scenarios are present, then scenario 2 takes priority.
+  it('scheduled (scenario 2) if there is a scheduled scenario, awaiting decision scenario, and not yet scheduled scenario', () => {
+    const scheduled = getMockPendingDetermination()
+    scheduled.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const now1 = new Date()
+    const aWeekFromNow = now1.setDate(now1.getDate() + 7)
+    const aWeekFromNowUtc = datetimeInUtc(aWeekFromNow)
+    scheduled.scheduleDate = formatFromApiGateway(aWeekFromNowUtc)
+
+    const awaitingDecision = getMockPendingDetermination()
+    awaitingDecision.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const now2 = new Date()
+    const aWeekAgo = now2.setDate(now2.getDate() - 7)
+    const aWeekAgoUtc = datetimeInUtc(aWeekAgo)
+    awaitingDecision.scheduleDate = formatFromApiGateway(aWeekAgoUtc)
+
+    const notYetScheduled = getMockPendingDetermination()
+    notYetScheduled.determinationStatus = ''
+    notYetScheduled.scheduleDate = ''
+    notYetScheduled.requestDate = 'not empty'
+
+    const result = identifyPendingDeterminationScenario([notYetScheduled, awaitingDecision, scheduled])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario2)
+    expect(result.pendingDetermination).toBe(scheduled)
+  })
+
+  // If scenarios 1 & 3 are present, then scenario 3 takes priority.
+  it('awaiting decision (scenario 3) if there is an awaiting decision scenario and not yet scheduled scenario', () => {
+    const awaitingDecision = getMockPendingDetermination()
+    awaitingDecision.determinationStatus = 'Random string' // Can be anything other than one of NonPendingDeterminationValues
+    const today = new Date()
+    const aWeekAgo = today.setDate(today.getDate() - 7)
+    const aWeekAgoUtc = datetimeInUtc(aWeekAgo)
+    awaitingDecision.scheduleDate = formatFromApiGateway(aWeekAgoUtc)
+
+    const notYetScheduled = getMockPendingDetermination()
+    notYetScheduled.determinationStatus = ''
+    notYetScheduled.scheduleDate = ''
+    notYetScheduled.requestDate = 'not empty'
+
+    const result = identifyPendingDeterminationScenario([notYetScheduled, awaitingDecision])
+    expect(result.scenarioType).toBe(ScenarioType.Scenario3)
+  })
+})
+
+/**
+ * Other tests
+ */
+
+// Test isDeterminationStatusPending()
+describe('Determination Status', () => {
   it('is pending if it is an empty string', () => {
+    const pendingDetermination = getMockPendingDetermination()
     expect(isDeterminationStatusPending(pendingDetermination)).toBe(true)
   })
 
   it('is pending if it is null', () => {
+    const pendingDetermination = getMockPendingDetermination()
     pendingDetermination.determinationStatus = null
     expect(isDeterminationStatusPending(pendingDetermination)).toBe(true)
   })
 
   it('is pending if it is undefined', () => {
+    const pendingDetermination = getMockPendingDetermination()
     pendingDetermination.determinationStatus = undefined
     expect(isDeterminationStatusPending(pendingDetermination)).toBe(true)
   })
 
   it('is pending if is missing', () => {
+    const pendingDetermination = getMockPendingDetermination()
     delete pendingDetermination.determinationStatus
     expect(isDeterminationStatusPending(pendingDetermination)).toBe(true)
   })
 
   it('is pending if it is not one of the non-pending values', () => {
+    const pendingDetermination = getMockPendingDetermination()
     pendingDetermination.determinationStatus = 'foo'
     expect(isDeterminationStatusPending(pendingDetermination)).toBe(true)
   })
 
   it('is not pending if it is one of the non-pending values', () => {
     for (const value of NonPendingDeterminationValues) {
+      const pendingDetermination = getMockPendingDetermination()
       pendingDetermination.determinationStatus = value
       expect(isDeterminationStatusPending(pendingDetermination)).toBe(false)
     }
@@ -163,5 +427,3 @@ describe('Comparing pending determination objects', () => {
     expect(result).toBe(false)
   })
 })
-
-// Test identifyPendingDeterminationScenario()
