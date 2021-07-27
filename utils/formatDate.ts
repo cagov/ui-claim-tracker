454e1ff86c819623b62f2a6697e80bd8ec0930e8
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-27 16:05:46 -0700

    Refactor time zone handling.

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,11 +1,19 @@
 /**
  * Utility file to handle datetimes.
  *
+ * Some helpful context from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date:
+ * > A JavaScript date is fundamentally specified as the number of milliseconds that have
+ * > elapsed since midnight on January 1, 1970, UTC. ... It's important to keep in mind
+ * > that while the time value at the heart of a Date object is UTC, the basic methods to
+ * > fetch the date and time or its components all work in the local (i.e. host system)
+ * > time zone and offset.
+ *
  * Because this is a State of California application, we assume all times are
  * in Pacific Time if there is no timezone provided in the datetime string.
  */
-import { format, isValid, parse } from 'date-fns'
-import { zonedTimeToUtc } from 'date-fns-tz'
+
+import { format, isValid } from 'date-fns'
+import { toDate } from 'date-fns-tz'
 import { ApiGatewayDateString } from '../types/common'
 
 const pacificTimeZone = 'America/Los_Angeles'
@@ -13,24 +21,33 @@ const apiGatewayFormat = "yyyy-MM-dd'T'HH:mm:ss"
 
 /**
  * Parse a date string from the API gateway.
+ *
+ * By default, if a date time string has no time zone (which is how we expect
+ * the date times from the API gateway to be formatted), it is interpreted as a UTC time.
+ *
+ * From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse:
+ *
+ * > For example, "2011-10-10" (date-only form), "2011-10-10T14:48:00" (date-time form),
+ * > or "2011-10-10T14:48:00.000+09:00" (date-time form with milliseconds and time zone)
+ * > can be passed and will be parsed. When the time zone offset is absent, date-only forms
+ * > are interpreted as a UTC time and date-time forms are interpreted as local time.
+ *
+ * This function will interpret date time strings with no time zone as a Pacific Time Zone
+ * time. If the date string argument contains a time zone offset, the `timeZone` option
+ * is ignored.
+ *
+ * See: https://github.com/marnusw/date-fns-tz#todate
  */
 export function parseApiGatewayDate(dateString: ApiGatewayDateString): Date {
-  return parse(dateString, apiGatewayFormat, new Date())
-}
-
-/**
- * Assume input is in Pacific Time and convert to UTC.
- */
-export function datetimeInUtc(date: Date | number | string): Date {
-  return zonedTimeToUtc(date, pacificTimeZone)
+  return toDate(dateString, { timeZone: pacificTimeZone })
 }
 
 /**
  * Parse and convert an API gateway string to UTC.
+ * @TODO: Delete
  */
 export function parseConvertDate(dateString: ApiGatewayDateString): Date {
-  const parsedDate = parseApiGatewayDate(dateString)
-  return datetimeInUtc(parsedDate)
+  return parseApiGatewayDate(dateString)
 }
 
 /**
@@ -43,11 +60,11 @@ export function formatFromApiGateway(date: Date): string {
 /**
  * Create a Date object that is offset from today.
  */
-export function getDateWithOffset(offset = 1): Date {
+export function getDateWithOffset(daysOffset = 1): Date {
   const today = new Date()
-  const sometime = today.setDate(today.getDate() + offset)
-  const sometimeUtc = datetimeInUtc(sometime)
-  return sometimeUtc
+  today.setDate(today.getDate() + daysOffset)
+  today.setHours(0, 0, 0, 0)
+  return today
 }
 
 /**
@@ -56,8 +73,7 @@ export function getDateWithOffset(offset = 1): Date {
 export function isValidDate(dateString: ApiGatewayDateString): boolean {
   let date: Date
 
-  // If the date format is such that it can't be parsed and converted to UTC,
-  // then it is definitely invalid.
+  // If the date format is such that it can't be parsed, then it is definitely invalid.
   try {
     date = parseConvertDate(dateString)
   } catch (error) {
@@ -65,9 +81,8 @@ export function isValidDate(dateString: ApiGatewayDateString): boolean {
   }
 
   if (isValid(date)) {
-    // Set a min date because it's possible for the date
-    // to be '0001-01-01'.
-    const minDate = datetimeInUtc('1900-01-01')
+    // Set a min date because it's possible for the date to be '0001-01-01'.
+    const minDate = toDate('1900-01-01', { timeZone: pacificTimeZone })
     if (date <= minDate) {
       return false
     }
@@ -92,7 +107,7 @@ export function isValidDate(dateString: ApiGatewayDateString): boolean {
  * Assumes that the given date is a valid date.
  */
 export function isDatePast(date: Date): boolean {
-  const today = datetimeInUtc(Date.now())
+  const today = new Date()
   return date < today
 }
 
