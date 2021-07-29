2b810d96f9b207d1df10c8117c3879ef0ef8b6c8
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-29 13:01:26 -0700

    Add Spanish language snapshots for appointment.

--- tests/components/ClaimStatus.test.tsx
+++ tests/components/ClaimStatus.test.tsx
@@ -1,6 +1,7 @@
 import MockDate from 'mockdate'
-import renderer from 'react-test-renderer'
+import renderer, { act } from 'react-test-renderer'
 
+import i18n from '../jest-i18n'
 import { ClaimStatus } from '../../components/ClaimStatus'
 import { Appointment, ClaimStatusContent } from '../../types/common'
 import apiGatewayStub from '../../utils/apiGatewayStub'
@@ -142,6 +143,7 @@ describe('Scenario 6', () => {
 /**
  * Appointment snapshot tests.
  */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 describe('If given an appointment', () => {
   beforeAll(() => {
     MockDate.set('2021-05-05')
@@ -152,6 +154,19 @@ describe('If given an appointment', () => {
       date: getDateWithOffset(0),
     }
     expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+  })
+
+  it('with no time slot, then match the snapshot, in Spanish', () => {
+    const appointment = {
+      date: getDateWithOffset(0),
+    }
+    act(() => {
+      i18n.changeLanguage('es')
+    })
+    expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+    act(() => {
+      i18n.changeLanguage('en')
+    })
   })
 
   it('with a morning time slot, then match the snapshot', () => {
@@ -165,6 +180,23 @@ describe('If given an appointment', () => {
     expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
   })
 
+  it('with a morning time slot, then match the snapshot, in Spanish', () => {
+    const appointment = {
+      date: getDateWithOffset(0),
+      timeSlot: {
+        rangeStart: 8,
+        rangeEnd: 10,
+      },
+    }
+    act(() => {
+      i18n.changeLanguage('es')
+    })
+    expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+    act(() => {
+      i18n.changeLanguage('en')
+    })
+  })
+
   it('with an afternoon time slot, then match the snapshot', () => {
     const appointment = {
       date: getDateWithOffset(0),
@@ -174,6 +206,23 @@ describe('If given an appointment', () => {
       },
     }
     expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+  })
+
+  it('with an afternoon time slot, then match the snapshot, in Spanish', () => {
+    const appointment = {
+      date: getDateWithOffset(0),
+      timeSlot: {
+        rangeStart: 1,
+        rangeEnd: 3,
+      },
+    }
+    act(() => {
+      i18n.changeLanguage('es')
+    })
+    expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+    act(() => {
+      i18n.changeLanguage('en')
+    })
   })
 
   it('with a time slot that starts in the morning and ends in the afternoon, then match the snapshot', () => {
@@ -187,6 +236,23 @@ describe('If given an appointment', () => {
     expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
   })
 
+  it('with a time slot that starts in the morning and ends in the afternoon, then match the snapshot, in Spanish', () => {
+    const appointment = {
+      date: getDateWithOffset(0),
+      timeSlot: {
+        rangeStart: 8,
+        rangeEnd: 3,
+      },
+    }
+    act(() => {
+      i18n.changeLanguage('es')
+    })
+    expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+    act(() => {
+      i18n.changeLanguage('en')
+    })
+  })
+
   it('with a time slot that has a nonsense time range, then match the snapshot', () => {
     const appointment = {
       date: getDateWithOffset(0),
@@ -197,4 +263,22 @@ describe('If given an appointment', () => {
     }
     expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
   })
+
+  it('with a time slot that has a nonsense time range, then match the snapshot, in Spanish', () => {
+    const appointment = {
+      date: getDateWithOffset(0),
+      timeSlot: {
+        rangeStart: 3,
+        rangeEnd: 9,
+      },
+    }
+    act(() => {
+      i18n.changeLanguage('es')
+    })
+    expect(testClaimStatus(ScenarioType.Scenario2, false, false, appointment)).toMatchSnapshot()
+    act(() => {
+      i18n.changeLanguage('en')
+    })
+  })
 })
+/* eslint-enable @typescript-eslint/no-floating-promises */
