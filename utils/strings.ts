5aaba29d875bb00bf0047460f0e93b925c5b275f
Rocket ! <rocketnova@users.noreply.github.com>
2021-07-29 18:00:49 -0700

    Add document-level comment for strings util.

--- utils/strings.ts
+++ utils/strings.ts
@@ -1,3 +1,7 @@
+/**
+ * Utility file for helper functions related to strings.
+ */
+
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
