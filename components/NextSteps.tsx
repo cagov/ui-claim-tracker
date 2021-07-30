aa96a72a80beeb0d42bf9606ecdd94078053a560
Kalvin <82277+kalvinwang@users.noreply.github.com>
2021-07-30 11:10:37 -0400

    Add NextStepsList component so we can recursively include nested bullets

--- components/NextSteps.tsx
+++ components/NextSteps.tsx
@@ -1,3 +1,4 @@
+import { NextStepsList } from './NextStepsList'
 import { Shimmer } from './Shimmer'
 import { TransLine } from './TransLine'
 import { TransLineContent } from '../types/common'
@@ -21,41 +22,7 @@ export const NextSteps: React.FC<NextStepsProps> = ({
   } else {
     loadableContent = (
       <div className="next-step-explanation">
-        <ul>
-          {nextSteps.map((nextStep, index) => {
-            // Use the only element, or the first element if an array, as the main bullet
-            // Include remaining elements in an array as sub-bullets
-            const thisStep = Array.isArray(nextStep) ? nextStep[0] : nextStep
-
-            return (
-              <li key={index} className="next-step">
-                <TransLine
-                  loading={loading}
-                  userArrivedFromUioMobile={userArrivedFromUioMobile}
-                  i18nKey={thisStep.i18nKey}
-                  links={thisStep.links}
-                />
-                {Array.isArray(nextStep) ? (
-                  <ul>
-                    {
-                      // Include sub-bullets; ignore first element since it was already included as a main bullet above
-                      nextStep.slice(1).map((subStep, index2) => (
-                        <li key={index2} className="next-step">
-                          <TransLine
-                            loading={loading}
-                            userArrivedFromUioMobile={userArrivedFromUioMobile}
-                            i18nKey={subStep.i18nKey}
-                            links={subStep.links}
-                          />
-                        </li>
-                      ))
-                    }
-                  </ul>
-                ) : null}
-              </li>
-            )
-          })}
-        </ul>
+        <NextStepsList loading={loading} userArrivedFromUioMobile={userArrivedFromUioMobile} nextSteps={nextSteps} />
       </div>
     )
   }
