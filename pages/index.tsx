2426519cf7aafb4ae02f3ce6afa01eab8fe6e3e2
Rocket ! <rocketnova@users.noreply.github.com>
2021-08-12 15:09:29 -0700

    Fix build issue with google fonts.

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,7 @@ export default function Home({
         <title>{t('title')}</title>
         <link rel="icon" href="/claimstatus/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
