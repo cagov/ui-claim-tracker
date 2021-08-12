42bef689287bccf1665a1c93acd118560e10120e
Kat Tipton <kat@navapbc.com>
2021-08-12 10:54:15 -0400

    Adds basic component tests for ClaimDetails

---
+++ tests/components/ClaimDetails.test.tsx
@@ -0,0 +1,105 @@
+import renderer from 'react-test-renderer'
+import { render, screen } from '@testing-library/react'
+import getScenarioContent, { ScenarioType } from '../../utils/getScenarioContent'
+import apiGatewayStub from '../../utils/apiGatewayStub'
+import { ScenarioContent, ClaimDetailsContent } from '../../types/common'
+
+import { useRouter } from 'next/router'
+
+import { ClaimDetails } from '../../components/ClaimDetails'
+
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn(),
+}))
+
+describe('ClaimDetails with all nonnull fields', () => {
+  it('shows all the fields', () => {
+    const mockRouter = {
+      locale: 'en',
+    }
+    ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
+
+    render(
+      <ClaimDetails
+        loading={false}
+        programType="Test Program Type"
+        benefitYear="Test Benefit Year"
+        claimBalance="Test Claim Balance"
+        weeklyBenefitAmount="Test Weekly Benefit Amount"
+        lastPaymentIssued="Test Last Payment Issued"
+        extensionType="Test Extension Type"
+      />,
+    )
+
+    expect(screen.queryByText('Claim Details')).toBeInTheDocument()
+    expect(screen.queryByText('Program Type')).toBeInTheDocument()
+    expect(screen.queryByText('Benefit Year')).toBeInTheDocument()
+    expect(screen.queryByText('Claim Balance')).toBeInTheDocument()
+    expect(screen.queryByText('Weekly Benefit Amount')).toBeInTheDocument()
+    expect(screen.queryByText('Last Payment Issued')).toBeInTheDocument()
+  })
+})
+
+describe('ClaimDetails with nullable fields', () => {
+  it('Null Claim Balance hides the field', () => {
+    const mockRouter = {
+      locale: 'en',
+    }
+    ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
+
+    render(
+      <ClaimDetails
+        loading={false}
+        programType="Test Program Type"
+        benefitYear="Test Benefit Year"
+        claimBalance={null}
+        weeklyBenefitAmount="Test Weekly Benefit Amount"
+        lastPaymentIssued="Test Last Payment Issued"
+        extensionType="Test Extension Type"
+      />,
+    )
+
+    expect(screen.queryByText('Claim Balance')).not.toBeInTheDocument()
+  })
+  it('Null Weekly Benefit Amount hides the field', () => {
+    const mockRouter = {
+      locale: 'en',
+    }
+    ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
+
+    render(
+      <ClaimDetails
+        loading={false}
+        programType="Test Program Type"
+        benefitYear="Test Benefit Year"
+        claimBalance="Test Claim Balance"
+        weeklyBenefitAmount={null}
+        lastPaymentIssued="Test Last Payment Issued"
+        extensionType="Test Extension Type"
+      />,
+    )
+
+    expect(screen.queryByText('Weekly Benefit Amount')).not.toBeInTheDocument()
+  })
+  it('Null Last Payment Issued hides the field', () => {
+    const mockRouter = {
+      locale: 'en',
+    }
+    ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
+
+    render(
+      <ClaimDetails
+        loading={false}
+        programType="Test Program Type"
+        benefitYear="Test Benefit Year"
+        claimBalance="Test Claim Balance"
+        weeklyBenefitAmount="Test Weekly Benefit Amount"
+        lastPaymentIssued={null}
+        extensionType="Test Extension Type"
+      />,
+    )
+
+    expect(screen.queryByText('Last Payment Issued')).not.toBeInTheDocument()
+  })
+})
