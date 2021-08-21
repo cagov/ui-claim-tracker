f4291c84ac52f963d925a587c82455d4e935bd50
Rocket ! <rocketnova@users.noreply.github.com>
2021-08-20 18:37:45 -0700

    Change isProd to isAzureEnv.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const { i18n } = require('./next-i18next.config')
 
-const isProd = process.env.NODE_ENV === 'production'
+const isAzureEnv = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // The Claim Tracker application must live at /claimstatus because IDM Webgate
   // expects the "context root" to be /claimstatus, and all other pages must be sub-paths.
   basePath: '/claimstatus',
-  assetPrefix: isProd ? '/claimstatus' : '',
+  assetPrefix: isAzureEnv ? '/claimstatus' : '',
   i18n,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.fallback = {
