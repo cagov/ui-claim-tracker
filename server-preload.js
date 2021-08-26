78bb93f03632068a533daf34d65a09ee0a5f75b8
Kalvin <82277+kalvinwang@users.noreply.github.com>
2021-08-26 12:16:33 -0400

    Update App Insights SDK call

--- server-preload.js
+++ server-preload.js
@@ -6,6 +6,10 @@
  * This script is called in package.json with:
  * - `node -r ./server-preload.js ./node_modules/.bin/next start -p 8080`
  *
+ * To test this locally,
+ * 1) update package.json yarn dev with
+ *   `node -r ./server-preload.js ./node_modules/.bin/next dev`
+ * 2) update the process.env.* below as appropriate (see App Service Application Settings for CONNECTION_STRING)
  *
  * References:
  * - https://jake.tl/notes/2021-04-04-nextjs-preload-hack
@@ -13,14 +17,42 @@
  */
 
 function setupLogging() {
+  // To disable Application Insights for an App Service env, set the Application Setting aka env var below to false
+  // AND set the Application Insights toggle in the web UI to disabled
+  if (process.env.APPINSIGHTS_DISABLE_SDK === 'disabled') {
+    return
+  }
+
+  const https = require('https')
+  const agent = new https.Agent({
+    keepAlive: true,
+    maxSockets: 24,
+    maxFreeSockets: 10,
+    timeout: 60000,
+    freeSocketTimeout: 30000,
+  })
+
+  // Only send Live Metrics when needed (e.g. prod); contributes to SNAT port exhaustion:
+  // https://github.com/microsoft/ApplicationInsights-node.js/issues/615
+  // Also, all process.env.* are cast to strings, so we avoid true/false values to minimize bugs
+  const enableLiveMetrics = process.env.APPINSIGHTS_ENABLE_LIVE_METRICS === 'enabled'
+
   // Connect to application insights during preload to capture HTTP requests.
   const appInsights = require('applicationinsights')
+
   appInsights
     .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
     .setAutoCollectRequests(true) // Default setting, but explicitly set to true for clarity
-    .setSendLiveMetrics(true) // Send Live Metrics to App Insights
+    .setSendLiveMetrics(enableLiveMetrics)
     .setAutoCollectConsole(true, true) // Pass the second argument to also log console.log() statements
-    .start()
+
+  // Force App Insights SDK to use configured agent: https://github.com/microsoft/ApplicationInsightsnode.js/issues/615
+  appInsights.defaultClient.config.httpsAgent = agent
+  if (enableLiveMetrics) {
+    appInsights.defaultClient.quickPulseClient.config.httpsAgent = agent
+  }
+
+  appInsights.start()
 }
 
 setupLogging()
