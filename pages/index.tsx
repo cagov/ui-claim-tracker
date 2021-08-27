a7f8b5fd3288be87a150b925eabbcb54cda5e861
Kalvin Wang <kalvinwang@users.noreply.github.com>
2021-08-27 08:03:21 -0700

    Update pages/index.tsx
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,9 +96,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res, locale,
   // Note: it's not possible to use the NEXT_PUBLIC_ prefix to expose these env vars to the browser
   // because they're set at build time, and Azure doesn't inject env vars ("App Settings") until runtime
   const URL_PREFIXES = {
-    urlPrefixUioDesktop: process.env.URL_PREFIX_UIO_DESKTOP || '',
-    urlPrefixUioMobile: process.env.URL_PREFIX_UIO_MOBILE || '',
-    urlPrefixBpo: process.env.URL_PREFIX_BPO || '',
+    urlPrefixUioDesktop: process.env.URL_PREFIX_UIO_DESKTOP ?? '',
+    urlPrefixUioMobile: process.env.URL_PREFIX_UIO_MOBILE ?? '',
+    urlPrefixBpo: process.env.URL_PREFIX_BPO ?? '',
   }
 
   // Other vars.
