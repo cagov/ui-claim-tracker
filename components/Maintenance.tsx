285cd6c4c2719d4b44ea3e6293b671e8e9980e20
Kalvin <82277+kalvinwang@users.noreply.github.com>
2021-09-03 12:11:33 -0400

    Update maintenance page content

--- components/Maintenance.tsx
+++ components/Maintenance.tsx
@@ -5,8 +5,9 @@ export const Maintenance: React.FC = () => {
 
   return (
     <div className="error-content">
-      <div className="error-label">{t('errors.server-error.label')}</div>
-      <div className="error-entry">{t('errors.server-error.entry')}</div>
+      <div className="error-label">{t('maintenance.label')}</div>
+      <div className="error-entry">{t('maintenance.entry')}</div>
+      <div className="error-entry">{t('maintenance.entry2')}</div>
     </div>
   )
 }
