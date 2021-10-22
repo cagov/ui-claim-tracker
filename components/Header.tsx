686406254b7d64e60b14f78323a9940f8fd214e6
Kat Tipton <kat@navapbc.com>
2021-10-22 09:57:41 -0400

    Adds status URL env handling

--- components/Header.tsx
+++ components/Header.tsx
@@ -52,7 +52,11 @@ export const Header: React.FC<HeaderProps> = ({ userArrivedFromUioMobile = false
           label={t('header.uio-history')}
           icon="ca-gov-icon-clock"
         />
-        <HeaderIcon link="/" label={t('header.uio-status')} icon="ca-gov-icon-file-medical-alt" />
+        <HeaderIcon
+          link={getUrl('uio-status-url', urlPrefixes)}
+          label={t('header.uio-status')}
+          icon="ca-gov-icon-file-medical-alt"
+        />
         <HeaderIcon
           link={getUrl('uio-desktop-profile-url', urlPrefixes)}
           label={t('header.uio-profile')}
