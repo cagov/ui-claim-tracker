47a0baf7300d3b6246a1050aae22b3075781d5cf
dtodd-edd <95440583+dtodd-edd@users.noreply.github.com>
2022-01-11 10:03:56 -0800

    Updated TextOptionalLink
    Updated hypertext to array of strings

--- types/common.ts
+++ types/common.ts
@@ -23,7 +23,7 @@ export interface TransLineContent {
 export interface TextOptionalLink {
   text: string
   links?: string[]
-  hypertext?: []
+  hypertext?: string[]
   subBullets?: TextOptionalLink[]
 }
 
